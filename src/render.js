bcec244b04e0a6e9a3e941e1d1a6cf9e7de8b21b
AlexTokarev <sasha.tokarev17@gmail.com>
2022-04-14 21:57:06 +0500

    Создание функции renderElement

--- src/render.js
+++ src/render.js
@@ -1,10 +1,27 @@
 export const RenderPosition = {
 	BEFOREBEGIN: 'beforebegin',
 	AFTERBEGIN: 'afterbegin',
-	BEFOREEND: 'beforeend',
+  BEFOREEND: 'beforeend',
 	AFTEREND:  'afterend',
 };
 
 export const renderTemplate = (container, template, place) => {
 	container.insertAdjacentHTML(place, template);
+};
+
+export const renderElement = (container, element, place) => {
+  switch (place){
+    case RenderPosition.BEFOREBEGIN:
+      container.before(element);
+      break;
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
 };
