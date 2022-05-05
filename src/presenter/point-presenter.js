a99b436accaf8019aee500788c3545d897cb2ce4
AlexTokarev <sasha.tokarev17@gmail.com>
2022-05-05 23:51:41 +0500

    Создание презенторов маршрута и точки маршрута

---
+++ src/presenter/point-presenter.js
@@ -0,0 +1,106 @@
+import EditPointView from '../view/edit-point-view';
+import WayPointView from '../view/way-point-view';
+import { render, RenderPosition, replace, remove } from '../utils/render.js';
+
+const Mode = {
+  DEFAULT: 'DEFAULT',
+  EDITING: 'EDITING',
+};
+
+
+export default class PointPresenter {
+  #eventListContainer = null;
+  #changeData = null;
+  #changeMode = null;
+
+  #eventComponent = null;
+  #eventEditComponent = null;
+
+  #event = null;
+  #mode = Mode.DEFAULT
+
+  constructor(eventListContainer, changeData, changeMode) {
+    this.#eventListContainer = eventListContainer;
+    this.#changeData = changeData;
+    this.#changeMode = changeMode;
+  }
+
+  init = (event) => {
+    this.#event = event;
+
+    const prevEventComponent = this.#eventComponent;
+    const prevEventEditComponent = this.#eventEditComponent;
+
+    this.#eventComponent = new WayPointView(event);
+    this.#eventEditComponent = new EditPointView(event);
+
+    this.#eventComponent.setEditClickHandler(this.#handleEditClick);
+    this.#eventComponent.setFavoriteClickHandler(this.#handleFavoriteClick);
+    //this.#eventComponent.setArchiveClickHandler(this.#handleArchiveClick);
+    this.#eventEditComponent.setFormSubmitHandler(this.#handleFormSubmit);
+
+    if (prevEventComponent === null || prevEventEditComponent === null) {
+      render(this.#eventListContainer, this.#eventComponent, RenderPosition.BEFOREEND);
+      return;
+    }
+
+    if (this.#mode === Mode.DEFAULT) { 
+      replace(this.#eventComponent, prevEventComponent);
+    }
+
+    if (this.#mode === Mode.EDITING) {
+      replace(this.#eventEditComponent, prevEventEditComponent);
+    }
+
+    remove(prevEventComponent);
+    remove(prevEventEditComponent);
+  }
+
+  destroy = () => {
+    remove(this.#eventComponent);
+    remove(this.#eventEditComponent);
+  }
+
+  resetView = () => {
+    if (this.#mode !== Mode.DEFAULT) {
+      this.#replaceFormToCard();
+    }
+  }
+
+  #replaceCardToForm = () => {
+    replace(this.#eventEditComponent, this.#eventComponent);
+    document.addEventListener('keydown', this.#escKeyDownHandler);
+    this.#changeMode();
+    this.#mode = Mode.EDITING;
+  };
+
+  #replaceFormToCard = () => {
+    replace(this.#eventComponent, this.#eventEditComponent);
+    document.removeEventListener('keydown', this.#escKeyDownHandler);
+    this.#mode = Mode.DEFAULT;
+  };
+
+  #escKeyDownHandler = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      this.#replaceFormToCard();
+    }
+  }
+
+  #handleEditClick = () => {
+    this.#replaceCardToForm();
+  }
+
+  #handleFavoriteClick = () => {
+    this.#changeData({ ...this.#event, isFavorite: !this.#event.isFavorite });
+  }
+
+  //#handleArchiveClick = () => {
+  //  this.#changeData({ ...this.#event, isArchive: !this.#event.isArchive });
+  //}
+
+  #handleFormSubmit = (event) => {
+    this.#changeData(event);
+    this.#replaceFormToCard();
+  }
+}
